68f4695afe402bc01b20fec861cd5f8bc7717ead
shbae <shbae@shbae-PC>
2019-05-21 11:26:13 +0900

    [배수현] 2일차 진행내용

---
+++ src/com/barunsw/ojt/day02/Person.java
@@ -0,0 +1,35 @@
+package com.barunsw.ojt.day02;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Person {
+	private int age;
+	private String name;
+	
+	public Person(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		//return String.format("age:%s, name:%s", age, name);
+		return ToStringBuilder.reflectionToString(this);
+	}
+}
--- src/com/barunsw/ojt/day02/StringTest.java
+++ src/com/barunsw/ojt/day02/StringTest.java
@@ -1,5 +1,77 @@
 package com.barunsw.ojt.day02;
 
-public class StringTest {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class StringTest {
+	private static final Logger LOGGER = LogManager.getLogger(StringTest.class);
+	
+	/**
+	 * main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LOGGER.debug("main");
+		
+		final Person onePerson = new Person(25, "홍길동");
+		LOGGER.debug("onePerson:" + onePerson);
+		
+		final String aaa = "Hello World";
+		if (aaa.startsWith("Hello")) {
+			LOGGER.debug("Hello로 시작");
+		}
+
+		if (aaa.endsWith("World")) {
+			LOGGER.debug("World로 끝");
+		}
+		
+		final String bbb = "Hello World";
+		//String bbb = new String("Hello World");
+		
+		if (aaa.equals(bbb)) {
+			LOGGER.debug("aaa와 bbb는 동일한 내용을 가진다.");
+		}
+		
+		if (aaa == bbb) {
+			LOGGER.debug("aaa와 bbb는 동일한 객체다.");
+		}
+		else {
+			LOGGER.debug("aaa와 bbb는 동일한 객체가 아니다.");
+		}
+		
+		final int niceIndex = aaa.indexOf("Nice");
+		LOGGER.debug("niceIndex:" + niceIndex);
+		
+		final int worldIndex = aaa.indexOf("World");
+		LOGGER.debug("worldIndex:" + worldIndex);
+		
+		final String ccc = "";
+		if (ccc.length() > 0) {
+			LOGGER.debug("CCC는 빈 스트링");
+		}
+		
+		if (ccc.isEmpty()) {
+			LOGGER.debug("CCC는 빈 스트링");
+		}
+		
+		String ddd = "aaa,bbb,ccc";
+		String[] splitList = ddd.split(",");
+		int index = 0;
+		for (String oneWord:splitList) {
+			LOGGER.debug(String.format("[%d]%s", index++, oneWord));
+		}
+		
+		String rawText = "\n\tHello World\n\n    ";
+		String trimText = rawText.trim();
+		LOGGER.debug(String.format("raw:[%s], trim:[%s]", rawText, trimText));
+		
+		String phoneNum = "010-1111-2222";
+		String regText = "(\\d+)-(\\d+)-(\\d+)";
+		if (phoneNum.matches(regText)) {
+			LOGGER.debug("정규표현식 일치");
+		}
+		else {
+			LOGGER.debug("정규표현식 일치하지 않음");
+		}
+	}
 }
