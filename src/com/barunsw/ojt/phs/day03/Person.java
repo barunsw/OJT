9660fb8d002ddbdbd6f5eef3f351629bd2827fb4
박희성 <forhfprh@barunsw.com>
2020-09-16 14:58:14 +0900

    [박희성] 3일차 ojt진행내용(File IO)

---
+++ src/com/barunsw/ojt/phs/day03/Person.java
@@ -0,0 +1,49 @@
+package com.barunsw.ojt.phs.day03;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Person implements Serializable{
+	private String name;
+	private String gender;
+	private String age;
+	private String phone;
+	private String address;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+}
