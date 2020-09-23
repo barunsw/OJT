fa20e037da03b8360541c1853d17b522cf76754c
박희성 <forhfprh@barunsw.com>
2020-09-23 19:11:50 +0900

    [박희성] OJT 8일차 진행내용 (Swing 주소록)

---
+++ src/com/barunsw/ojt/phs/day08/AddressBookDAO.java
@@ -0,0 +1,11 @@
+package com.barunsw.ojt.phs.day08;
+
+import java.util.List;
+
+public interface AddressBookDAO {
+
+	public void loadAllList() throws Exception;
+
+	public void saveAllList() throws Exception;
+
+}
