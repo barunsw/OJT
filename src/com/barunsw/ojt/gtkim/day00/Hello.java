f1e5c749c59dfeca45e40ab521dd976dfb2ea617
김균태 <rbsxo1268@barunsw.com>
2021-08-25 09:58:08 +0900

    feature/-1 not fork test

---
+++ src/com/barunsw/ojt/gtkim/day00/Hello.java
@@ -0,0 +1,12 @@
+package com.barunsw.ojt.gtkim.day00;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello branch");
+	}
+}
