0e84b4096be858d10f28b2d972be0483d9e69d91
jgmoon <gmfd78459123@gmail.com>
2025-02-03 15:05:43 +0900

    [문종근] 6일차 sql 수정

--- src/com/barunsw/ojt/mjg/day06/DBTest.java
+++ src/com/barunsw/ojt/mjg/day06/DBTest.java
@@ -29,7 +29,7 @@ public class DBTest {
             LOGGER.debug("전체 데이터 조회");
             List<AddressBookVo> addressList = mapper.selectAddressList(null);
             addressList.forEach(address -> LOGGER.debug(address));
-        
+
             // 2. 새로운 주소 정보 생성
             LOGGER.debug("새로운 주소 정보 생성");
             AddressBookVo insertAddress = new AddressBookVo();
@@ -77,18 +77,35 @@ public class DBTest {
             addressList.forEach(address -> LOGGER.debug("삭제 후 데이터: " + address));
 	
             SchedulerManager.startAgeUpdateScheduler();
-            
-            // 5. 성이 "문"인 사람 조회
+
+            // 5. 성이 '문'인 사람 조회
             LOGGER.debug("성이 '문'인 사람 조회");
             addressList = mapper.selectByLastNameMoon(null);
-            // addressList.forEach(address -> LOGGER.debug("문 씨 데이터: " + address));
             
             if (addressList.isEmpty()) {
                 LOGGER.debug("문씨인 사람이 없습니다.");
             } 
             else {
-                addressList.forEach(address -> LOGGER.debug("문 씨 데이터: " + address));
+                addressList.forEach(address -> LOGGER.debug("문 씨 데이터: " + address + "\n"));
             }
+          
+			// 6. 성이 입력받은 값으로 시작하는 사람 조회
+			LOGGER.debug("성이 입력받은 값으로 시작하는 사람 조회 (바뀐 방식, 입력값 적용)");
+			
+			// AddressBookVo 객체 생성 및 name 속성을 '김'으로 설정
+			AddressBookVo searchCriteria = new AddressBookVo();
+			searchCriteria.setName("김");
+			
+			// searchCriteria 객체를 전달하면 XML 동적 SQL에서 name이 null이 아니므로 "김%" 조건으로 검색됨.
+			addressList = mapper.selectByLastNameMoon(searchCriteria);
+			
+			if (addressList.isEmpty()) {
+			     LOGGER.debug("[" + searchCriteria.getName() + "] 씨인 사람이 없습니다.");
+			} 
+			else {
+		    // 입력받은 값("김")을 로그 메시지에 포함하여 출력
+			addressList.forEach(address -> LOGGER.debug("[" + searchCriteria.getName() + "] 씨 데이터: " + address));
+			}
             
             // 변경 사항 커밋
             session.commit();
